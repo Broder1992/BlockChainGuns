da365f6ab21919ee1118eb7b3064add3f40a7a4d
Örjan Larsson <rjan Larsson>
2017-04-02 18:51:56 +0200

    Created the write() function and altered the invoke()

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -58,10 +58,30 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	}else if function == "write" {
+		return t.write(stub, args);
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, value string
+	var err error
+	ftm.Println("Running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2 name of the variable and value not set.")
+	}
+
+	name = args[0];
+	value = args[1];
+	err = stub.PutState(name, []byte(value))
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
 }
 
 // Query is our entry point for queries
